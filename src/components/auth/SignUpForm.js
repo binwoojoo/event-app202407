14a12714885b6bcd3333bb19bbbf2d5c541d7ca1
binwoojoo <oooo940212@gmail.com>
2024-07-12 10:32:45 +0900

    인증코드 검증 사용자 피드백

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import styles from "./SignUpForm.module.scss";
-import EmailInput from "./EmailInput";
-import VerificationInput from "./VerificationInput";
-import ProgressBar from "../ProgressBar";
+import React, { useState } from 'react';
+import styles from './SignUpForm.module.scss';
+import EmailInput from './EmailInput';
+import VerificationInput from './VerificationInput';
+import ProgressBar from '../ProgressBar';
 
 const SignUpForm = () => {
+
   // 현재 몇 단계가 진행되고 있는지
   const [step, setStep] = useState(1);
 
   // 단계가 성공적으로 완료되었는지
   const [success, setSuccess] = useState(false);
 
-  // 이메일 중복 확인이 끝났을 때 호출될 함수
-  const emailSuccessHandler = () => {
+  // 입력된 이메일
+  const [enteredEmail, setEnteredEmail] = useState('');
+
+  // 이메일 중복확인이 끝났을 때 호출될 함수
+  const emailSuccessHandler = (email) => {
 
     setSuccess(true);
+    setEnteredEmail(email);
 
     setTimeout(() => {
       setStep(2);
@@ -24,16 +29,15 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className={styles.SignUpForm}>
+    <div className={styles.signupForm}>
       <div className={styles.formStepActive}>
 
-        {/* {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
+        {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
 
-        {step === 2 && <VerificationInput />} */}
-
-        <VerificationInput />
+        {step === 2 && <VerificationInput email={enteredEmail} />}
 
         {success && <ProgressBar />}
+
       </div>
     </div>
   );
