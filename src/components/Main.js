5b1e6d421ce23ac855ce00e5f082ec04bed81d35
oooowb <oooo940212@gmail.com>
2024-07-16 10:56:43 +0900

    로그아웃

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { useRouteLoaderData } from 'react-router-dom';
+import React from "react";
+import { Form, useRouteLoaderData } from "react-router-dom";
 
 const Main = () => {
-
-  const { role, email } = useRouteLoaderData('user-data');
+  const { role, email } = useRouteLoaderData("user-data");
 
   return (
     <>
       <h2>{email}님 환영합니다.</h2>
       <h3>현재 권한: [ {role} ]</h3>
-      <button>Logout</button>
+      {/* 다른 라우트의 액션을 트리거하는 방법 */}
+      <Form action="/logout" method="POST">
+        <button>Logout</button>
+      </Form>
     </>
   );
 };
--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { NavLink, useRouteLoaderData } from 'react-router-dom';
-import styles from './MainNavigation.module.scss';
+import React from "react";
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
+import styles from "./MainNavigation.module.scss";
 
 const MainNavigation = () => {
-
-  const userData = useRouteLoaderData('user-data');
+  
+  const userData = useRouteLoaderData("user-data");
 
   const activeFn = ({ isActive }) => {
     // NavLink 컴포넌트에 className프롭스에 함수를 전달하면
@@ -18,27 +18,22 @@ const MainNavigation = () => {
       <nav>
         <ul className={styles.list}>
           <li>
-            <NavLink
-              to=""
-              className={activeFn}
-              end
-            >
+            <NavLink to="" className={activeFn} end>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="events"
-              className={activeFn}
-            >
+            <NavLink to="events" className={activeFn}>
               Events
             </NavLink>
           </li>
 
           {userData && (
-            <li>
-              {userData && <button style={{ width: '100%' }}>Logout</button>}
-            </li>
+            <Form action="/logout" method="POST">
+              <li>
+                {userData && <button style={{ width: "100%" }}>Logout</button>}
+              </li>
+            </Form>
           )}
         </ul>
       </nav>
