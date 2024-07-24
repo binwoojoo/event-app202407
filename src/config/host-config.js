f801c4c689167fc28605df0137d306230b3bb41a
binwoojoo <161430796+binwoojoo@users.noreply.github.com>
2024-07-24 11:25:10 +0900

    주소 수정ㅋㅋ

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -10,7 +10,7 @@ let backendHostName;
 if (clientHostName === "localhost") {
   backendHostName = "http://localhost:" + LOCAL_PORT;
 } else if (clientHostName === "my-event-app4444.s3-website.ap-northeast-2.amazonaws.com/") {
-  backendHostName = "http://43.203.239.67";
+  backendHostName = "http://43.201.108.115";
 }
 
 const API_BASE_URL = backendHostName;
